ffcfb6e79daa0806de1ad56c7f7ffa29ab115877
Max <57811248+MaxTribouillard@users.noreply.github.com>
2024-11-24 14:57:00 +0100

    font tester fini a 90%
    fini a 90%, reste qlq bug sur le form

--- docs/page/td5/font-tester.php
+++ docs/page/td5/font-tester.php
@@ -30,5 +30,15 @@ if ($_GET["color"] != "blue" && $_GET["color"] && $_GET['color'] != "green" && $
 //
 // MAINTENANT ON AJOUTE UN FORMULAIRE POUR PLUS DE PERSONNALISATION
 
+echo "<form>";
+echo "<label for='color'>Choose a color</label>";
+echo "<input type='text' id='color' placeholder='Color' name='color' aria-placeholder='Choose a color'>";
+echo "<label for='size'>Choose a size</label>";
+echo "<input type='text' id='size' name='size' placeholder='Choose a size'>";
+echo "<label for='message'>Message</label>";
+echo "<input type='text' id='message' name='message' placeholder='Type a mesage'>";
+echo "<input type='submit' id='send' name='send' value='Envoyer' href='font-tester.php'>";
+echo "</form>";
+
 
 ?>
