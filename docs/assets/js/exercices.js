99453a5a03d3b05a80205f75b09c5c666ffb80eb
Max <57811248+MaxTribouillard@users.noreply.github.com>
2024-12-02 23:48:02 +0100

    debut exo 1
    phase de test d'exo 1 pas fini

---
+++ docs/assets/js/exercices.js
@@ -0,0 +1,7 @@
+function backgroundRed() {
+    document.body.style.backgroundColor = 'red';
+}
+
+document.getElementById('boutonRouge').addEventListener("click", function(){
+    console.log('toto')
+})
