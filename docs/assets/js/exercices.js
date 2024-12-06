bdb48fc33ef8ca9fb45c54ca7abe16d47039e5a9
Max <57811248+MaxTribouillard@users.noreply.github.com>
2024-12-06 21:26:44 +0100

    qlq modif sur css exo 1 et exo 2 presque fini

--- docs/assets/js/exercices.js
+++ docs/assets/js/exercices.js
@@ -1,7 +1,7 @@
 // EXERCICE 1
 
 let boutonRouge = document.getElementById('rouge'); // Par id
-let boutonVert = document.getElementById('vert') // Par id également
+let boutonVert = document.getElementById('vert') 
 
 // fonction pour choisir plus faciliment la couleur et éviter les répétitions
 
@@ -18,3 +18,19 @@ boutonVert.addEventListener("click", function(){
 
 // EXERCICE 2
 
+function addElementToList(){
+
+    
+    var info = document.getElementById('info');
+    var addValue = info.value.trim();
+
+        if(addValue != ""){
+
+            var list = document.getElementById("list");
+            let li = document.createElement('li');
+            li.textContent = addValue;
+            list.appendChild(li);
+
+        }
+}
+    
